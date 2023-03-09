984872da46b2f0d33b1d46f4ba6880453b676c92
João Henrique Terêncio <94974380+Jh-Terencio@users.noreply.github.com>
2023-03-09 15:07:03 -0300

    Update lerArquivos.c

--- Capítulo 11 do livro/lerArquivos.c
+++ Capítulo 11 do livro/lerArquivos.c
@@ -12,7 +12,8 @@ int main(void){
     } else {
         printf("%-10s%-13s%s\n", "numConta", "nome", "saldoConta");
         fscanf(cfPtr, "%d%s%f", &numConta, &nome, &saldoConta);
-
+        
+        //Nesse caso o eof fecha quando o arquivo termina, como ele recebe como argumento o ponteiro para o arquivo, quando esse arquivo termina o eof fecha.
         while(!feof(cfPtr)){
             printf("%-10s%-13s%s\n", "numConta", "nome", "saldoConta");
             fscanf(cfPtr, "%d%s%f", &numConta, &nome, &saldoConta);
